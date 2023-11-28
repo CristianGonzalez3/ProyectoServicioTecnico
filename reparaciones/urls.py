fbfe7f3393c17222812bdd89e7227ccdf2b58c1b
Cristian Gaston Gonzalez <cristiang0212@gmail.com>
2023-11-27 21:12:35 -0300

    cambion en pantalla

--- reparaciones/urls.py
+++ reparaciones/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('inicio/', views.inicio, name="inicio"),
     path('pagina_principal_usuario/', views.pagina_principal_usuario, name='pagina_principal_usuario'),
     path('pagina_principal_tecnico/', views.pagina_principal_tecnico, name='pagina_principal_tecnico'),
-    path('perfil/', views.perfil, name='perfil'),
     path('pagina_reparaciones/', views.pagina_reparaciones, name='pagina_reparaciones'),
     path('reparaciones_agregar/', views.reparaciones_agregar, name='reparaciones_agregar'),
 ]
